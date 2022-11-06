99b88c2b4ce73ba0546eca15db85117bdb0737ff
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-06 19:32:58 +0700

    Update main.cpp

--- prisoner's dilemma/src/main.cpp
+++ prisoner's dilemma/src/main.cpp
@@ -1,57 +1,58 @@
+
 #include <iostream>
 #include <vector>
 #include <string>
-//#include "strategies.cpp"
-
-class GameInfo {
-private:
-	unsigned int nSteps;
-	unsigned int nPlayers;
-	char mode; // d = detailed, f = fast, t = tournament
-	std::string configPath;
-	std::string matrixFile;
-	std::vector<int> condition; //contains current game-matrix, the score
-	std::vector<int> rules; //contains rules of score
-public:
-	GameInfo() : nSteps(0), nPlayers(0), mode('d'), condition(), rules(),
-		configPath(), matrixFile() {};
-	GameInfo(unsigned int nstps, unsigned int nplrs, std::string cnfg, std::string mtx, char md = 'd') :
-		nSteps(nstps), nPlayers(nplrs), configPath(cnfg), matrixFile(mtx), mode(md), condition() {
-		rules.resize(6);
-		rules = { 0, 7, 3, 1, 9, 5 }; //    DD   CC   CD  
-									  // C  0    7    3
-									  // D  1    9    5
-	}
-
-	void print_info() const {
-		for (int i = 0; i < condition.size(); i++) {
-			std::cout << i;
-			if (i % 3 == 0)
-				std::cout << '\n';
-		}
-	}
-	const std::vector<int>& get_info() const { return condition; }
-	const std::vector<int>& get_rules() const { return rules; }
-
-	~GameInfo(){};
-};
-
-class Player {
-protected:
-	const GameInfo& game;
-public:
-	virtual char make_step() = 0;
-	virtual ~Player() {}
-};
+#include <map>
+#include <memory>
+#include <list>
+#include "gamestate.cpp"
+#include "strategies.cpp"
 
 class arbitrator {
 private:
-	std::vector<Player*> players;
-	GameInfo& game;
+	GameState* game;
 public:
+	arbitrator(GameState* gm): game(gm) {}
+
 	void round() {};
+	void new_player(strategies strat) { 
+		PlayerFabric fabric;
+		std::shared_ptr<Player> newPlayer = fabric.make_player(strat);
+	}
 };
 
-int main(){
-	return 0;
+strategies str_to_enum(char* s) {
+	std::string str(s);
+	if (str == "alldefect")
+		return strategies::alldefect;
+	else if (str == "allcooperate")
+		return strategies::allcooperate;
+}
+
+void parse(int argc, char** argv, char* mode, unsigned int* steps, std::string* cnfg, std::string* mtrx, std::vector<strategies> strats) {
+	size_t i = 1;
+	for (i; i < argc && argv[i][0] != '-'; i++) //reading only strategies
+		strats.push_back(str_to_enum(argv[i]));
+	if (i < 4) {
+		std::cout << "There is not enough participants\n";
+		exit(0);
+	}
+	for (i; i < argc; i++) {
+
+	}
+
+}
+
+int main(int argc, char** argv){
+	std::vector<strategies> strats;
+	strats.reserve(3);
+
+	char mode = 'd';
+	unsigned int steps = 0;
+	std::string configPath;
+	std::string matrixPath;
+
+	parse(argc, argv, &mode, &steps, &configPath, &matrixPath, &strats);
+	GameState dilemma(steps, configPath, matrixPath, mode);
+
 }
