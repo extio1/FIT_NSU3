69659c579d464c05680f5f994219e241f56a7b85
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-07 17:58:02 +0700

    Update gamestate.cpp

--- prisoner's dilemma/src/gamestate.cpp
+++ prisoner's dilemma/src/gamestate.cpp
@@ -1,84 +1,58 @@
+#pragma once
 #include <vector>
 #include <string>
 #include <map>
 #include <iostream>
-#include "strategies.cpp"
-extern class Player;
-extern enum class strategies;
+#include "gamestate.h"
+#include "arbitrator.h"
+#include "strategies.h"
 
-class GameState;
-class arbitrator {
-private:
-	GameState& game;
-public:
-	arbitrator(GameState& gm) : game(gm) {}
-
-	//опрашивает выбор игроков, по вектору их выборов roundResult формирует очки и добавляет в GameStat.condition
-	void round() { 
-		std::vector<char> roundResult;
-		roundResult.reserve(game.get_players().size());
-		for (auto player : game.get_players())
-			roundResult.push_back(player->make_step(game));
-		//тут должен быть цикл, который берет все различные тройки и начисляет им очки по матрице GameStat.rules
-	};
-	~arbitrator() {}
-};
-
-class GameState {
-private:
-	unsigned int nSteps;
-	char mode; // d = detailed, f = fast, t = tournament
-	std::string configPath;
-	std::string matrixFile;
-	std::vector<int> condition; //contains current game-matrix, the score
-	std::map<std::string, std::string> rules; //contains rules of score
-	std::vector<std::shared_ptr<Player>> players; //the vector of strategies
-public:
-	GameState() : nSteps(0), mode('d'), condition(), rules(),
+GameState::GameState() : nSteps(0), mode('d'), condition(), rules(),
 		configPath(), matrixFile() {};
 
-	GameState(unsigned int nstps, char md = 'd') :
+GameState::GameState(unsigned int nstps, char md) :
 		nSteps(nstps), mode(md), condition() {}
 
-	GameState(unsigned int nstps, std::string cnfg, std::string mtx, char md = 'd') :
-		nSteps(nstps), configPath(), matrixFile(), mode(md), condition() {
-		if (!cnfg.empty()) {
-			//do something
-		}
-		if (!mtx.empty()) {
-			//make other rules
-		}
-		else {
-			rules = { {"CCC", "777"}, {"CCD", "339"}, {"CDC", "393"}, {"DCC", "933"},
-					  {"CDD", "055"}, {"DCD", "505"}, {"DDC", "550"}, {"DDD", "111"} };
-		}
+GameState::GameState(unsigned int nstps, std::string cnfg, std::string mtx, char md) :
+	nSteps(nstps), configPath(), matrixFile(), mode(md), condition() {
+	if (!cnfg.empty()) {
+		//do something
 	}
-
-	void print_info() const {
-		for (int i = 0; i < condition.size(); i++) {
-			std::cout << i;
-			if (i % 3 == 0)
-				std::cout << '\n';
-		}
+	if (!mtx.empty()) {
+		//make other rules
 	}
-
-	const std::vector<int>& get_info() const { return condition; }
-	const std::vector<std::shared_ptr<Player>> get_players() const { return players; }
-	const std::map<std::string, std::string>& get_rules() const { return rules; }
-
-	void add_players(std::vector<strategies>& strategs) {
-		PlayerFabric fabric;
-		for (strategies str : strategs) {
-			std::shared_ptr<Player> newPlayer = fabric.make_player(str);
-			players.push_back(newPlayer);
-		}
+	else {
+		rules = { {"CCC", "777"}, {"CCD", "339"}, {"CDC", "393"}, {"DCC", "933"},
+					{"CDD", "055"}, {"DCD", "505"}, {"DDC", "550"}, {"DDD", "111"} };
 	}
+}
 
-	void start() {
-		arbitrator arb(*this);
-		for (int i = 0; i < nSteps; i++)
-			arb.round();
+void GameState::print_info() const {
+	for (size_t i = 0; i < condition.size(); i++) {
+		std::cout << i;
+		if (i % 3 == 0)
+			std::cout << '\n';
 	}
+}
 
-	~GameState() {};
-};
+const std::vector<int>& GameState::get_info() const { return condition; }
+const std::vector<std::shared_ptr<Player>> GameState::get_players() const { return players; }
+const std::map<std::string, std::string>& GameState::get_rules() const { return rules; }
+
+void GameState::add_players(std::vector<strategies>& strategs) {
+	PlayerFabric fabric;
+	for (strategies str : strategs) {
+		std::shared_ptr<Player> newPlayer = fabric.make_player(str);
+		players.push_back(newPlayer);
+	}
+}
+
+void GameState::start() {
+	arbitrator arb(*this);
+	for (size_t i = 0; i < nSteps; i++)
+		arb.round();
+}
+
+GameState::~GameState() {};
+
+
