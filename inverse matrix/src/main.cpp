c8acc2c655c7aad20d6c378472f4fa545935ce3c
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-07 21:36:21 +0700

    Create main.cpp

---
+++ inverse matrix/src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <time.h>
+#include "matrix.h"
+using namespace std;
+
+
+int main() {
+	int m;
+	int n;
+	cout << "Enter the matrix size (n x n) :\n";
+	cin >> n;
+	cout << "Enter the accuracy parameter (integer) :\n";
+	cin >> m;
+	cout << "Enter the matrix :\n";
+	Matrix aMat(n);
+	aMat.entry_arr();
+
+	timespec start, end;
+	if (timespec_get(&start, TIME_UTC) != -1);
+	Matrix inverseA = inverse_matrix(aMat, m);
+	if(timespec_get(&end, TIME_UTC != -1));
+
+	cout << "----------------------------------------" << endl;
+	inverseA.print_matrix();
+	cout << "----------------------------------------" << endl;
+
+	cout << "Calculatuing time: " << (end.tv_sec - start.tv_sec) + 0.000000001 * (end.tv_nsec - start.tv_nsec) << "sec. ";
+
+	return 0;
+}
