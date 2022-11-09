73173a5a99aec620d494762e9c812fcc3f2b206e
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-09 21:36:17 +0700

    Update arbitrator.h

--- prisoner's dilemma/src/arbitrator.h
+++ prisoner's dilemma/src/arbitrator.h
@@ -1,11 +1,19 @@
 #pragma once
+#include <vector>
+#include <memory>
+#include <string>
+
 class GameState;
+class Player;
+enum class strategies;
 
 class arbitrator {
 private:
 	GameState& game;
+	std::vector<std::shared_ptr<Player>> players;
 public:
 	arbitrator(GameState&);
-	void round();
+	void create_players(const std::vector<strategies>&);
+	std::vector<int> round(std::string* = nullptr);
 	~arbitrator();
 };
