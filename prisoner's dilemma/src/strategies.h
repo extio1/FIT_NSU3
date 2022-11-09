c60bd5690af5f181545cc7d3a33bfd2fb1b1abf0
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-09 22:33:14 +0700

    Update strategies.h

--- prisoner's dilemma/src/strategies.h
+++ prisoner's dilemma/src/strategies.h
@@ -1,32 +1,42 @@
 #pragma once
 #include <string>
 #include <memory>
+#include <vector>
 
 class GameState;
-enum class strategies { alldefect, allcooperate, titfortat };
+enum class strategies { alldefect, allcooperate, titfortat, grim };
 
 class Player {
 public:
 	virtual char make_step() = 0;
+	virtual void enter_score(const std::vector<int>& score);
+	virtual void enter_choice(const std::vector<char>& choice);
 	//virtual ~Player() = 0;
 };
 
 class alldefect : public Player {
 public:
 	char make_step();
-	~alldefect();
 };
 
 class allcooperate : public Player {
 public:
 	char make_step();
-	~allcooperate();
 };
 
 class titfortat : public Player {
 public:
 	char make_step();
-	~titfortat();
+};
+
+class grim : public Player {
+private:
+	std::vector<char> prevGameChoice;
+	bool grimTrigger;
+public:
+	grim(): grimTrigger(false){}
+	char make_step();
+	void enter_choice(const std::vector<char>& choice);
 };
 
 class PlayerFabric {
