261108e17fbc66d7a2dc725d5410d186b4f2bd56
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-09 22:32:59 +0700

    Update strategies.cpp

--- prisoner's dilemma/src/strategies.cpp
+++ prisoner's dilemma/src/strategies.cpp
@@ -4,20 +4,44 @@
 
 #include "strategies.h"
 
+void Player::enter_choice(const std::vector<char>& choice) {
+	return;
+}
+void Player::enter_score(const std::vector<int>& score) {
+	return;
+}
+
 char alldefect::make_step() {
 	return 'D';
 }
-alldefect::~alldefect(){}
 
 char allcooperate::make_step() {
 	return 'C';
 }
-allcooperate::~allcooperate() {}
 
 char titfortat::make_step() {
 	return 'C';
 }
-titfortat::~titfortat() {}
+
+char grim::make_step() {
+	if (grimTrigger) {
+		return 'D';
+	}
+	else {
+		for (char ch : prevGameChoice) {
+			if (ch == 'D') {
+				grimTrigger = true;
+				return 'D';
+			}
+		}
+	}
+
+	return 'C';
+}
+void grim::enter_choice(const std::vector<char>& choice) {
+	prevGameChoice = choice;
+}
+
 
 std::shared_ptr<Player> PlayerFabric::make_player(strategies strat) {
 	if (strat == strategies::alldefect)
@@ -26,6 +50,8 @@ std::shared_ptr<Player> PlayerFabric::make_player(strategies strat) {
 		return std::make_shared<allcooperate>();
 	else if (strat == strategies::titfortat)
 		return std::make_shared<titfortat>();
+	else if (strat == strategies::grim)
+		return std::make_shared<grim>();
 	else
 		return nullptr;
 }
