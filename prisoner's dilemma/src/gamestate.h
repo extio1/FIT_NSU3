b1fd4061aa23df8edfee454fe7d4959d8f23e3ff
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-09 21:36:44 +0700

    Update gamestate.h

--- prisoner's dilemma/src/gamestate.h
+++ prisoner's dilemma/src/gamestate.h
@@ -8,23 +8,28 @@ class Player;
 enum class strategies;
 
 class GameState {
+public:
+
+	GameState();
+	GameState(unsigned int, char = 'd');
+	GameState(unsigned int, std::string, std::string, char md = 'd');
+
+	void print_info() const;
+	const std::vector<int>& get_info() const;
+	const std::map<std::string, std::string>& get_rules() const;
+
+	void start(const std::vector<strategies>&, const char mode = 'd');
+	void renew_score(const std::vector<int>&);
+
+	~GameState();
+
 private:
+
 	unsigned int nSteps;
 	char mode; // d = detailed, f = fast, t = tournament
 	std::string configPath;
 	std::string matrixFile;
-	std::vector<int> condition; //contains current game-matrix, the score
+	std::vector<int> score; //contains the current score
 	std::map<std::string, std::string> rules; //contains rules of score
-	std::vector<std::shared_ptr<Player>> players; //the vector of strategies
-public:
-	GameState();
-	GameState(unsigned int, char = 'd');
-	GameState(unsigned int, std::string, std::string, char md = 'd');
-	void print_info() const;
-	const std::vector<int>& get_info() const;
-	const std::vector<std::shared_ptr<Player>> get_players() const;
-	const std::map<std::string, std::string>& get_rules() const;
-	void add_players(std::vector<strategies>&);
-	void start();
-	~GameState();
+
 };
