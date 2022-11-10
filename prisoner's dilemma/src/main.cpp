13124998a26459a7bb5959d3bd1a891a8e546900
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-10 20:39:55 +0700

    Update main.cpp

--- prisoner's dilemma/src/main.cpp
+++ prisoner's dilemma/src/main.cpp
@@ -4,46 +4,28 @@
 #include <map>
 #include <memory>
 #include <list>
-//#include <C:\boost_1_80_0\boost\algorithm\>
+//#include <boost\program_options.hpp>
 
 #include "strategies.h"
 #include "gamestate.h"
 
-strategies str_to_enum(char* s) {
-	std::string str(s);
-	if (str == "alldefect")
-		return strategies::alldefect;
-	else if (str == "allcooperate")
-		return strategies::allcooperate;
-}
-/*
-//парсер недописан
-void parse(int argc, char** argv, char* mode, unsigned int* steps, std::string* cnfg, std::string* mtrx, std::vector<strategies> strats) {
-	size_t i = 1;
-	for (i; i < argc && argv[i][0] != '-'; i++) //reading only strategies
-		strats.push_back(str_to_enum(argv[i]));
-	if (i < 4) {
-		std::cout << "There is not enough participants\n";
-		exit(0);
-	}
 
-	for (i; i < argc; i++) {
-	}
-}*/
+
+//парсер недописан
+
 
 //mode, скорее всего, не надо хранить в gamestate
 int main(int argc, char** argv) {
-	
-	std::vector<strategies> strats = { strategies::allcooperate, strategies::alldefect, strategies::titfortat, strategies::grim };
+	//boost::program_options::options_description desc("General options");
+
+	std::vector<strategies> strats = { strategies::titfortat, strategies::grim, strategies::allcooperate };
 	strats.reserve(3);
-	char mode = 't';
-	unsigned int steps = 6;
+	char mode = 'f';
+	int steps = 10;
 	std::string configPath;
-	std::string matrixPath;
-	/*
-	//parse(argc, argv, &mode, &steps, &configPath, &matrixPath, &strats);
-	//GameState dilemma(steps, configPath, matrixPath, mode);
-	*/
+	std::string matrixPath = "matrix1.csv";
+
+	//GameState dilemma(argc, argv);
 	GameState dilemma(steps, configPath, matrixPath, mode);
 	dilemma.start(strats, mode);
 }
