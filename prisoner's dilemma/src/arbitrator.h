20f250ea6a4f4dfe3365b2bdb4d6de88c2412edb
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-13 18:24:51 +0700

    Update arbitrator.h

--- prisoner's dilemma/src/arbitrator.h
+++ prisoner's dilemma/src/arbitrator.h
@@ -22,7 +22,6 @@ public:
 	size_t who_winner() const;
 
 	void create_players(const std::vector<strategies>&);
-	void round_tour(std::string* = nullptr);
 
 	void match();
 	void match(const size_t, const size_t, const size_t, const int);
