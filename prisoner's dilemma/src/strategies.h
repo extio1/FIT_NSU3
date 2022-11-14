d9d8f0f010c33601ebcf55860cfe508e7b255aee
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-14 17:33:13 +0700

    Update strategies.h

--- prisoner's dilemma/src/strategies.h
+++ prisoner's dilemma/src/strategies.h
@@ -12,32 +12,32 @@ public:
 	virtual void enter_score(const std::vector<int>& score);
 	virtual void enter_choice(const std::vector<char>& choice);
 	virtual void clear();
+	virtual ~Player() = 0;
 };
 
-class random : public Player {
+class random : public Player { //рандом, что еще сказать
 public:
 	char make_step();
 };
 
 class alldefect : public Player { //всегда кооперироваться
-
 public:
 	char make_step();
 };
 
 class allcooperate : public Player { //всегда предавать
-
 public:
 	char make_step();
 };
 
 class titfortat : public Player { // "глаз за глаз", если кто-то предал, то мстит, иначе кооперируется. Первый ход всегда не предаёт
 private:						  // кроме того имеет шанс в ~2% скооперироваться, несмотря на то, что кто-то предал
- 	std::vector<char> prevGameChoice;
+	std::vector<char> prevGameChoice;
 public:
 	char make_step();
 	void enter_choice(const std::vector<char>& choice);
 	void clear();
+	~titfortat();
 };
 
 class grim : public Player { //обидчивая стратегия, если кто-то предал, предаёт до конца игры
@@ -49,6 +49,7 @@ public:
 	char make_step();
 	void enter_choice(const std::vector<char>& choice);
 	void clear();
+	~grim();
 };
 
 class detective : public Player { //первые 4-е хода CDCD, если со строны противника было предательство, будет вести себя как
@@ -62,6 +63,7 @@ public:
 	char like_titfortat();
 	char make_step();
 	void enter_choice(const std::vector<char>& choice);
+	~detective();
 };
 
 class PlayerFabric {
