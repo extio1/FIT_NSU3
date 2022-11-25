0df95d8d289f86c18edee28c532e155eead46e73
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-25 12:56:10 +0700

    Update main.c

--- cache/main.c
+++ cache/main.c
@@ -1,21 +1,70 @@
-#include <stdlib.h>
+	#include <stdlib.h>
 #include <stdio.h>
-#include <intrin.h>
+#include <intrin.h> //для __rdtsc()
+#include "matrix.h" //нахождение обратной матрицы - для прогрева процессора
+#include <iostream>
+#include <time.h>
 
-void direct_init(long* arr) {
-
+void print(long* arr, const int size) {
+	for (int i = 0; i < size; i++)
+		printf("%d ", arr[i]);
 }
 
-void back_init(long* arr) {
-
+void direct_init(long* arr, const int size) {
+	arr[size - 1] = 0;
+	for (int i = 0; i < size - 1; i++) {
+		arr[i] = i + 1;
+	}
 }
 
-void rand_init(long* arr) {
-
+void back_init(long* arr, const int size) {
+	arr[0] = size - 1;
+	for (int i = size - 1; i > 0 ; i--) {
+		arr[i] = i - 1;
+	}
 }
 
-void test(long* arr) {
-	//прогрев процессора
+void warm_proc() { //работает чуть больше секунды
+	Matrix warm(50);
+	warm.read_from_file("mat.txt");
+	inverse_matrix(warm, 3000);
+}
+
+void random_init(long* arr, const int size) {
+	for (int i = 0; i < size; i++) //помечаем, что ни в какие вершины еще не зашли
+		arr[i] = -1;
+
+	int cur_pos = 0;
+	unsigned long long rnd;
+	//на size-1 позицию кладется индекс еще не участвующего в обходе элемента; новый индекс отличен от текущего индекса
+	for (int i = 0; i < size-1; i++) { //индекс следующего элемента случайно вычисляется с помощью счетчика тактов
+		do {
+			rnd = __rdtsc() % size;
+		} while (arr[rnd] != -1 || rnd == cur_pos);
+		arr[cur_pos] = rnd;
+		cur_pos = rnd;
+	}
+	arr[cur_pos] = 0;
+}
+
+void test(const long* arr, const int size) {
+	const int k = 5; //число повторений цикла для замера среднего времени доступа к элементу массива
+	unsigned long long start, end;
+
+	warm_proc();
+
+	for (int i = 0, k = 0; i < size; i++) { //загрузили в кэш массив
+		k = arr[k];
+	}
+
+	start = __rdtsc();
+	for (int i = 0, k = 0; i < size*k; i++) {
+		k = arr[k];
+	}
+	end = __rdtsc();
+
+	printf("Time: ");
+	printf("%llu", (end - start));
 }
 
 int main() {
@@ -23,23 +72,23 @@ int main() {
 	int nmax = 8388608; //sizeof(long) * 8388608 = 32Mb
 
 
-	long* arr = (long*) malloc(sizeof(long) * nmin);
-	long* arr_dir = (long*) malloc(sizeof(long) * nmin);
-	long* arr_back = (long*) malloc(sizeof(long) * nmin);
-	long* arr_rand = (long*) malloc(sizeof(long) * nmin);
-
+	long* arr = (long*)malloc(sizeof(long) * nmin);
+	long* arr_dir = (long*)malloc(sizeof(long) * nmin);
+	long* arr_back = (long*)malloc(sizeof(long) * nmin);
+	long* arr_rand = (long*)malloc(sizeof(long) * nmin);
+	direct_init(arr, nmin);
+	test(arr, nmin);
+	//print(arr, nmin);
+	/*
 	int n = nmin;
 	while (n < nmax) {
-
-		direct_init(arr);
+		direct_init(arr, n);
 		test(arr);
-		back_init(arr);
+		back_init(arr, n);
 		test(arr);
-		rand_init(arr);
+		rand_init(arr, n);
 		test(arr);
-
 		n *= 1.1;
-
 		long* p;
 		if ((p = (long*)realloc(arr, sizeof(long) * n)) != NULL) {
 			arr = p;
@@ -49,7 +98,7 @@ int main() {
 			exit(1);
 		}
 	}
-
+	*/
 	free(arr_dir);
 	free(arr_back);
 	free(arr_rand);
