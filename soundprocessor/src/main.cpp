393500c0cfee643fdc6e47ef2d2be1f67b5edb21
extio1 <99738815+extio1@users.noreply.github.com>
2022-11-30 18:16:48 +0700

    Update main.cpp

--- soundprocessor/src/main.cpp
+++ soundprocessor/src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "wavinfo.h"
+#include "audiofile.h"
+#include "convasciiint.h"
 
 struct configs {
     std::string configpath;
@@ -16,6 +17,30 @@ struct parsedata {
 
 int main()
 {
-    wavinfo wav_file;
-    wav_file.read_header("district_four.wav");
+    //std::fstream a;
+    //a.open("district_four.wav");
+
+   // audiofile wav;
+   // wav.read_header("district_four.wav");
+    audiofile inwav("district_four.wav");
+
+    audiofile file;
+    file.create_audiofile("output.wav");
+
+    char arr[4];
+
+    /*
+    int_to_ascii_seq_le(arr, 81298312, 4);
+    printf("%d\n", ascii_seq_to_int_le(arr, 4));
+
+    for (int i = 0; i < 4; i++) {
+        printf("%d ", (arr[i] < 0) ? arr[i] + 256 : arr[i]);
+    }
+    */
+    //audiofile outwav("output.wav");
+
+    //soundproc proc;
+   // proc.use("effectname", inwav, outwav);
+
 }
+
