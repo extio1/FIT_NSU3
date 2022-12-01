ebb5358ca582652a204b19a899f991780abeb424
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-01 22:12:10 +0700

    Update data.cpp

--- soundprocessor/src/data.cpp
+++ soundprocessor/src/data.cpp
@@ -1,5 +1,7 @@
 #include "audiofile.h"
 #include "convasciiint.h"
+#include "writer.h"
+
 #include <fstream>
 #include <vector>
 
@@ -9,7 +11,7 @@ void wavdata::test() {
 	std::cout << data_pos_begin << ' ' << cur_pos << '\n';
 }
 
-wavdata::wavdata(std::fstream& in): size_data_chunk(512), eof(false) {
+void wavdata::find_begin(std::ifstream& in) {
 	int saved_position = in.tellg();
 	in.seekg(0);
 
@@ -39,27 +41,53 @@ wavdata::wavdata(std::fstream& in): size_data_chunk(512), eof(false) {
 	in.seekg(saved_position);
 }
 
-void wavdata::read(std::vector<int>& outarr, std::fstream& f) {
+void wavdata::read(std::vector<int>& outarr, std::ifstream& f) {
 	char buffer[4];
+
+	if(outarr.size() != size_data_chunk)
+		outarr.resize(size_data_chunk);
+
+	if (f.tellg() != cur_pos)
+		f.seekg(cur_pos);
 
 	int new_pos = 0;
 	for (int i = 0; i < size_data_chunk; i++) {
-		if (cur_pos + new_pos + 1 <= data_size) {
-			f.read(buffer, 4);
-			outarr[new_pos++] = ascii_seq_to_int_le(buffer, 4);
+		if (cur_pos + new_pos <= data_size) {
+			for (int j = 0; j < 2; j++) {
+				f.get(buffer[j]);
+			}
+			outarr[new_pos++] = ascii_seq_to_int_le(buffer, 2);
 		}
 		else {
 			eof = true;
 			break;
 		}
 	}
-	if (cur_pos + new_pos + 1 < data_size) {
-		cur_pos += new_pos + 1;
+
+
+	if (cur_pos + (new_pos + 1) * 2 < data_size) {
+		cur_pos += new_pos * 2;
 	}
 	else {
 		cur_pos = data_pos_begin;
+		eof = true;
 	}
 }
 
+
+void wavdata::set_begin(unsigned int begin_pos) {
+	data_pos_begin = begin_pos;
+	cur_pos = begin_pos;
+}
+
+void wavdata::write(std::vector<int>& arr, std::ofstream& f) {
+	writer writer;
+	for (int i = 0; i < arr.size() && arr[i] != -1; i++)
+		writer.write_sample(arr[i], f);
+}
+
+bool wavdata::endof() {
+	return eof;
+}
 
 wavdata::~wavdata() {}
