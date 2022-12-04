8bafe478057331bb6dd3b8c2fa53b13abfe4f136
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-04 20:46:43 +0700

    Update writer.cpp

--- soundprocessor/src/writer.cpp
+++ soundprocessor/src/writer.cpp
@@ -1,17 +1,6 @@
 #include "writer.h"
 #include "convasciiint.h"
 
-namespace {
-	inline void normalize_2_b(int& num) {
-		if (num > 32760) {
-			(num) = 32760;
-		}
-		else if (num < -32760) {
-			(num) = -32760;
-		}
-	}
-}
-
 writer::writer() : n_bytes_sample(2) { buffer.resize(n_bytes_sample); }
 writer::writer(int s) : n_bytes_sample(s) { buffer.resize(n_bytes_sample); }
 
@@ -22,8 +11,7 @@ void writer::write_sample(const int num, std::ofstream& f) {
 }
 
 void writer::write_num_b(const int num, std::ofstream& f, const int n_bytes) {
-	if (buffer.size() < n_bytes)
-		buffer.resize(n_bytes);
+	buffer.resize(n_bytes);
 
 	int_to_ascii_seq(buffer, num, n_bytes);
 	for (int i = 0; i < n_bytes; i++)
