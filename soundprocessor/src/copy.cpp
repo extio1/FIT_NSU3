fab6ee53e935107d08d73bbde917146e6773746e
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:58:14 +0700

    Create copy.cpp

---
+++ soundprocessor/src/copy.cpp
@@ -0,0 +1,22 @@
+#include "converter.h"
+#include "audiofile.h"
+#include "configfileparser.h"
+#include "soundproc.h"
+
+void copy::launch(const command_info& com) {
+	inaudiofile inf(com.input[0]);
+	outaudiofile outf(com.output);
+
+	std::vector<unsigned int> temp;
+
+	while (!inf.eof()) {
+		inf >> temp;
+		outf << temp;
+	}
+}
+
+void copy::who_am_i() {
+	std::cout << "=====================================================\n";
+	std::cout << "$ copy \nCopies previous file to output file. \n";
+	std::cout << "=====================================================\n\n";
+}
