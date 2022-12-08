4b7390d73df7f2e7196e416ede784b1b9508605b
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:55:58 +0700

    Update sample.h

--- soundprocessor/src/sample.h
+++ soundprocessor/src/sample.h
@@ -2,17 +2,25 @@
 class sample {
 public:
 	sample();
-	sample(int);
+	sample(unsigned int);
 	sample(const sample&);
 	
 	sample& operator=(const sample&);
 	sample& operator+=(const sample&);
+	sample& operator/=(const sample&);
+	sample& operator*=(int);
+	sample& operator*=(float);
 
-	operator int() const;
+	operator unsigned int() const;
 
 	~sample();
 private:
+	void normalize();
+	void normalize(int&);
 	int data;
 };
 
 sample operator+(const sample&, const sample&);
+sample operator/(const sample&, const sample&);
+sample operator*(const sample&, int);
+sample operator*(const sample&, float);
