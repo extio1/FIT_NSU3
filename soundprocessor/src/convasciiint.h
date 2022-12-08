4bd745b5abc4732319e3ac26f5a0796c7432f5fd
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:52:33 +0700

    Update convasciiint.h

--- soundprocessor/src/convasciiint.h
+++ soundprocessor/src/convasciiint.h
@@ -2,7 +2,7 @@
 #include<vector>
 
 int little_to_big_endian_byte(int);
-int ascii_seq_to_int_le(const char*, const int); //устаревшее  - надо везде, где используется заменить на то, что ниже
+int ascii_seq_to_int_le(const char*, const int);
 int ascii_seq_to_int(const char*, const int);
-void int_to_ascii_seq_le(char*, const unsigned int, const int); //устаревшее  - надо везде, где используется заменить на то, что ниже
+void int_to_ascii_seq_le(char*, const unsigned int, const int);
 void int_to_ascii_seq(std::vector<char>&, const unsigned int, const int);
