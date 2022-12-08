e503500ec294c831ed52021f62ec915cc8424084
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:53:44 +0700

    Update data.cpp

--- soundprocessor/src/data.cpp
+++ soundprocessor/src/data.cpp
@@ -1,23 +1,25 @@
 #include "audiofile.h"
 #include "convasciiint.h"
 #include "writer.h"
+#include "excpts.h"
 
 #include <fstream>
 #include <vector>
+#include <memory>
 
-wavdata::wavdata(): size_data_chunk(512), eof(false), cur_pos(0), data_pos_begin(0), data_size(0) {}
+wavdata::wavdata(): writer(std::make_unique<WavWriter>()), size_data_chunk(512), bytes_per_sample(2), cur_pos(0), data_pos_begin(0), data_size(0) {}
 
-void wavdata::test() {
-	std::cout << data_pos_begin << ' ' << cur_pos << '\n';
+void wavdata::skip(int size) {
+	cur_pos += size * bytes_per_sample;
 }
 
-void wavdata::find_begin(std::ifstream& in) {
+void wavdata::find_begin(std::ifstream& in, const char* name) {
 	int saved_position = in.tellg();
 	in.seekg(0);
 
 	char ch = 0;
 	int not_found_begin_counter = 0;
-	while (not_found_begin_counter < 4) {
+	while (not_found_begin_counter < 4 && !in.eof()) {
 		in >> ch;
 		if (ch == 'd' && not_found_begin_counter == 0)
 			not_found_begin_counter++;
@@ -25,11 +27,17 @@ void wavdata::find_begin(std::ifstream& in) {
 			not_found_begin_counter++;
 		else if (ch == 't' && not_found_begin_counter == 2)
 			not_found_begin_counter++;
-		else if (ch == 'a' && not_found_begin_counter == 3)
+		else if (ch == 'a' && not_found_begin_counter == 3) {
 			not_found_begin_counter++;
+			break;
+		}
 		else
 			not_found_begin_counter = 0;
 	}
+
+	bool data_was_found = (not_found_begin_counter == 4);
+	if (!data_was_found)
+		throw wrong_header(name, "data");
 
 	char buffer[4];
 	in.read(buffer, 4);
@@ -41,53 +49,33 @@ void wavdata::find_begin(std::ifstream& in) {
 	in.seekg(saved_position);
 }
 
-void wavdata::read(std::vector<int>& outarr, std::ifstream& f) {
+void wavdata::read(std::vector<unsigned int>& outarr, std::ifstream& f) {
 	char buffer[4];
-
+	
 	if(outarr.size() != size_data_chunk)
 		outarr.resize(size_data_chunk);
-
 	if (f.tellg() != cur_pos)
 		f.seekg(cur_pos);
 
 	int new_pos = 0;
 	for (int i = 0; i < size_data_chunk; i++) {
-		if (cur_pos + new_pos <= data_size) {
-			for (int j = 0; j < 2; j++) {
-				f.get(buffer[j]);
-			}
-			outarr[new_pos++] = ascii_seq_to_int_le(buffer, 2);
+		if (!f.eof()) {
+			f.read(buffer, bytes_per_sample);
+			outarr[new_pos++] = ascii_seq_to_int_le(buffer, bytes_per_sample);
 		}
 		else {
-			eof = true;
+			outarr.resize(i);
 			break;
 		}
 	}
 
-
-	if (cur_pos + (new_pos + 1) * 2 < data_size) {
-		cur_pos += new_pos * 2;
-	}
-	else {
-		cur_pos = data_pos_begin;
-		eof = true;
-	}
+	cur_pos += new_pos * bytes_per_sample;
 }
 
 
-void wavdata::set_begin(unsigned int begin_pos) {
-	data_pos_begin = begin_pos;
-	cur_pos = begin_pos;
-}
-
-void wavdata::write(std::vector<int>& arr, std::ofstream& f) {
-	writer writer;
-	for (int i = 0; i < arr.size() && arr[i] != -1; i++)
-		writer.write_sample(arr[i], f);
-}
-
-bool wavdata::endof() {
-	return eof;
+void wavdata::write(std::vector<unsigned int>& arr, std::ofstream& f) {
+	for (unsigned int i : arr)
+		writer->write_sample(i, f);
 }
 
 wavdata::~wavdata() {}
