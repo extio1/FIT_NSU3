fdfc4681b3eccbed560f17e6ea5c61a8f9b07e34
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:56:21 +0700

    Update writer.cpp

--- soundprocessor/src/writer.cpp
+++ soundprocessor/src/writer.cpp
@@ -1,16 +1,16 @@
 #include "writer.h"
 #include "convasciiint.h"
 
-writer::writer() : n_bytes_sample(2) { buffer.resize(n_bytes_sample); }
-writer::writer(int s) : n_bytes_sample(s) { buffer.resize(n_bytes_sample); }
+WavWriter::WavWriter() : n_bytes_sample(2) { buffer.resize(n_bytes_sample); }
+WavWriter::WavWriter(int s) : n_bytes_sample(s) { buffer.resize(n_bytes_sample); }
 
-void writer::write_sample(const int num, std::ofstream& f) {
+void WavWriter::write_sample(const int num, std::ofstream& f) {
 	int_to_ascii_seq(buffer, num, n_bytes_sample);
 	for (int i = 0; i < n_bytes_sample; i++)
 		f << buffer[i];
 }
 
-void writer::write_num_b(const int num, std::ofstream& f, const int n_bytes) {
+void WavWriter::write_num_b(const int num, std::ofstream& f, const int n_bytes) {
 	buffer.resize(n_bytes);
 
 	int_to_ascii_seq(buffer, num, n_bytes);
