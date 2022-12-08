979da1e6ae0f8f353f324747de49c8183289b6e9
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:55:05 +0700

    Update main.cpp

--- soundprocessor/src/main.cpp
+++ soundprocessor/src/main.cpp
@@ -8,76 +8,64 @@
 #include "convasciiint.h"
 #include "commandlineparser.h"
 #include "configfileparser.h"
+#include "soundproc.h"
 
 
 int main(int argc, char** argv) {
-    comdata data;/*
+    comdata data;
     try {
         parse_command_line(argc, argv, data);
-        check_correct_wav(data.wav_file_path);
+        check_correct_wav(data.input_path);
     }
-    catch (not_enough_arguments nof) {
-        std::cout << "0";
-        std::cerr << nof.what();
+    catch (wrong_num_arguments nof) { //ошибки парсера
+        std::cerr << nof.how_many() << " arguments is not enoung to launch -" << nof.what_for() << '\n';
+        std::cerr << "at least:\n0 for -h\n3 for -c";
         exit(nof.get_error_code());
     }
     catch (unknown_command uc) {
-        std::cout << "1";
         std::cerr << uc.what();
+        std::cerr << uc.what_mode() << " is not known.\n";
         exit(uc.get_error_code());
     }
     catch (command_line_parser_error clpe) {
-        std::cout << "2";
         std::cerr << clpe.what();
         exit(clpe.get_error_code());
     }
-    catch (input_wav_format_exception iwfe) {
-        std::cout << "3";
-        std::cerr << iwfe.what();
+    catch (file_havent_opened fho) { //ошибки сообщающие о неправильных форматах файлов
+        std::cerr << "File <" << fho.what_file() << "> haven't opened.";
+        exit(fho.get_error_code());
+    }
+    catch (wrong_header wh) {
+        std::cerr << "File <" << wh.what_file() << "> have error in <" << wh.what_section() << "> section.\n";
+        exit(wh.get_error_code());
+    }
+    catch (wav_file_error iwfe) {
+        std::cerr << "Some error occurred in <" << iwfe.what_file() << "> file";
         exit(iwfe.get_error_code());
     }
-    catch (file_havent_opened fho) {
-        std::cout << "4";
-        std::cerr << fho.what();
-        exit(fho.get_error_code());
-    }
     catch (...) {
-        std::cout << "ERROR\n";
+        std::cerr << "Unexpected error.";
     }
-    std::cout << "fine";
-    */
-    data.mode = 'c';
-    data.config_path = "config.txt";
-    data.output_path = "output.txt";
-    data.wav_file_path.push_back("district_four.wav");
 
+    soundproc proc;
+    if (data.mode == 'h') {
+        proc.help();
+        return 0;
+    }
+
+    configparser cnfgparser;
     try {
-        configparser configparser(data.config_path);
-        soundproc proc;
-        proc.use(configparser.next_command(data));
+        cnfgparser.open(data.config_path);
     }
-    catch (file_havent_opened fho) {
-        std::cout << "4";
-        std::cerr << fho.what();
+    catch(file_havent_opened fho){
+        std::cerr << "Error while opening config file <" << fho.what_file() <<">.\n";
         exit(fho.get_error_code());
     }
-    catch (...) {
-        std::cerr << "ERROR";
-    }
+
+    while (!cnfgparser.end_of_config()){
+        proc.use(cnfgparser.next_command(data));
+    } 
     
-
-    /*
-    command_info test;
-    test.name = "mute";
-    test.int_param.push_back(5);
-    test.int_param.push_back(6);
-    test.input.push_back("district_four.wav");
-    test.output = "output.wav";
-
-    proc.use(test);
-    std::string test = "nan";
-    int a = atoi(test.c_str());
-    std::cout << a;
-    */
+    std::cout << "\n-----------==========Done!==========-----------\n";
     return 0;
 }
