c0b33bce0f07f2311467d1d325088a9d8acdafc5
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:54:15 +0700

    Update excpts.h

--- soundprocessor/src/excpts.h
+++ soundprocessor/src/excpts.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 
-class command_line_parser_error : public std::exception {
+class command_line_parser_error : public std::exception { //ошибка парсера - общее
 public:
 	virtual const char* what() const noexcept override;
 	virtual int get_error_code() const noexcept;
@@ -9,12 +9,14 @@ private:
 	static const int error_code = 10;
 };
 
-class not_enough_arguments : public command_line_parser_error {
+class wrong_num_arguments : public command_line_parser_error { //ошибка парсера - некорректный набор параметров
 public:
-	not_enough_arguments();
-	not_enough_arguments(int);
-	not_enough_arguments(int, const char);
-	const char* what() const noexcept final;
+	wrong_num_arguments();
+	wrong_num_arguments(int);
+	wrong_num_arguments(int, const char);
+	const char* what() const noexcept;
+	int how_many() const noexcept;
+	char what_for() const noexcept;
 	int get_error_code() const noexcept;
 private:
 	static const int error_code = 11;
@@ -22,38 +24,87 @@ private:
 	char mode;
 };
 
-class unknown_command : public command_line_parser_error {
+class unknown_command : public command_line_parser_error { //ошибка парсера - неизвезстый режим работы (не -c или -h)
 public:
 	unknown_command();
 	unknown_command(const char);
-	const char* what() const noexcept final;
+	const char* what() const noexcept;
+	char what_mode() const noexcept;
 	int get_error_code() const noexcept;
 private:
 	static const int error_code = 12;
 	char wrong_mode;
 };
 
-class input_wav_format_exception : public std::exception {
+class wav_file_error : public std::exception { //общая ошибка связанная с wav файлом
 public:
-	virtual const char* what() const noexcept override;
+	wav_file_error();
+	wav_file_error(const char*);
+	const char* what() const noexcept override;
+	const char* what_file() const noexcept;
 	virtual int get_error_code() const noexcept;
+protected:
+	const char* filename;
 private:
 	static const int error_code = 20;
 };
-/*
-class wrong_header : public input_wav_format_exception {
+
+class wrong_header : public wav_file_error { //неправильная wav заговочная часть
 public:
+	wrong_header();
+	wrong_header(const char*, const char*);
+	using wav_file_error::what_file;
 	const char* what() const noexcept override;
+	const char* what_section() const noexcept;
 	int get_error_code() const noexcept override;
 private:
+	const char* section;
 	static const int error_code = 21;
 };
-*/
 
-class file_havent_opened : public std::exception {
+class file_havent_opened : public std::exception { //файл не был открыл
 public:
+	file_havent_opened();
+	file_havent_opened(const char*);
+	const char* what() const noexcept override;
+	const char* what_file() const noexcept;
+	int get_error_code() const noexcept;
+private:
+	const char* name;
+	static const int error_code = 30;
+};
+
+class converter_error : public std::exception {
+public:
+	converter_error();
+	converter_error(const char*);
+	const char* what() const noexcept override;
+	const char* what_name() const noexcept;
+	int get_error_code() const noexcept;
+protected:
+	const char* conv_name;
+private:
+	static const int error_code = 40;
+};
+
+class conv_wrong_name : public converter_error {
+public:
+	conv_wrong_name();
+	conv_wrong_name(const char*);
+	using converter_error::what_name;
 	const char* what() const noexcept override;
 	int get_error_code() const noexcept;
 private:
-	static const int error_code = 30;
+	static const int error_code = 41;
+};
+
+class conv_not_enought_args : public converter_error {
+public:
+	conv_not_enought_args();
+	conv_not_enought_args(const char*);
+	using converter_error::what_name;
+	const char* what() const noexcept override;
+	int get_error_code() const noexcept;
+private:
+	static const int error_code = 42;
 };
