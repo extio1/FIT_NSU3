b941caef4eef1ad6f8d43be826729d2e7bc31dec
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:54:37 +0700

    Update header.cpp

--- soundprocessor/src/header.cpp
+++ soundprocessor/src/header.cpp
@@ -9,30 +9,29 @@ wavheader::wavheader() : average_bytes_per_sec(0), block_align(0),
 						 comp_code(0), n_of_channels(0), sample_rate(0), 
 						 sign_bits_per_sample(0), RIFF_chunk_size(0) {}
 
-void wavheader::read_header(std::ifstream& wavin) {
+void wavheader::read_header(std::ifstream& wavin, const char* filename) {
 	char buffer[5];
 	buffer[4] = '\0';
 
 	wavin.read(buffer, 4);
 	
-	//if (!std::string(buffer).compare("RIFF"))
 	if(std::string(buffer).compare("RIFF"))
-		throw input_wav_format_exception();
+		throw wrong_header(filename, buffer);
 
 	wavin.read(buffer, 4);
 	RIFF_chunk_size = ascii_seq_to_int_le(buffer, 4);
 
 	wavin.read(buffer, 4);
 	if (std::string(buffer).compare("WAVE"))
-		throw input_wav_format_exception();
+		throw wrong_header(filename, buffer);
 
 	wavin.read(buffer, 4);
 	if (std::string(buffer).compare("fmt "))
-		throw input_wav_format_exception();
+		throw wrong_header(filename, buffer);
 
 	wavin.read(buffer, 4);
 	if (ascii_seq_to_int_le(buffer, 4) != 16)
-		throw input_wav_format_exception();
+		throw wrong_header(filename, buffer);
 
 	wavin.read(buffer, 2);
 	comp_code = ascii_seq_to_int_le(buffer, 2);
@@ -53,82 +52,8 @@ void wavheader::read_header(std::ifstream& wavin) {
 	sign_bits_per_sample = ascii_seq_to_int_le(buffer, 2);
 }
 
-void wavheader::write_header(std::ofstream& wavout) {}
-	/*
-	if (wavout.is_open()) {
-		wavout << 'R' << 'I' << 'F' << 'F';
-		//std::cout << buffer << '\n'; //RIFF chunkId
-
-		wavout.read(buffer, 4);
-		//std::cout << ascii_seq_to_int_le(buffer, 4) << '\n'; //chunkSize
-
-		wavout.read(buffer, 4);
-		//std::cout << buffer << '\n'; //WAVE format
-
-		wavout.read(buffer, 4);
-		//std::cout << buffer << '\n'; //fmt subchunk1Id
-
-		//все что сверху должно быть всегда, если нет, кинуть исключение
-
-		wavout.read(buffer, 4);
-		//std::cout << ascii_seq_to_int_le(buffer, 4) << '\n'; //размер чанка fmt, должен быть равен 16
-
-		wavout.read(buffer, 2);
-		//std::cout << ascii_seq_to_int_le(buffer, 2) << '\n';
-		comp_code = ascii_seq_to_int_le(buffer, 2);
-
-		wavout.read(buffer, 2);
-		//std::cout << ascii_seq_to_int_le(buffer, 2) << '\n';
-		n_of_channels = ascii_seq_to_int_le(buffer, 2);
-
-		wavout.read(buffer, 4);
-		//std::cout << ascii_seq_to_int_le(buffer, 4) << '\n';
-		sample_rate = ascii_seq_to_int_le(buffer, 4);
-
-		wavout.read(buffer, 4);
-		//std::cout << ascii_seq_to_int_le(buffer, 4) << '\n';
-		average_bytes_per_sec = ascii_seq_to_int_le(buffer, 4);
-
-		wavout.read(buffer, 2);
-		//std::cout << ascii_seq_to_int_le(buffer, 2) << '\n';
-		block_align = ascii_seq_to_int_le(buffer, 2);
-
-		wavout.read(buffer, 2);
-		//std::cout << ascii_seq_to_int_le(buffer, 2) << '\n';
-		sign_bits_per_sample = ascii_seq_to_int_le(buffer, 2);
-
-		wavout.read(buffer, 4);
-		//std::cout << buffer << '\n'; //LIST?
-
-		if (!strcmp(buffer, "LIST")) { //если есть какая-то доп.информация. То запишем ее в отдельный txt файл
-			std::ofstream extrainfo("extrainformation.txt");
-			if (extrainfo.is_open()) {
-				wavout.read(buffer, 4);
-				int size = ascii_seq_to_int_le(buffer, 4);
-				for (int i = 0; i < size; ++i) {
-					extrainfo << wavout.get();
-				}
-			}
-			else {
-				std::cerr << "Error while writing extra information about .wav file.";
-			}
-		}
-
-		wavout.read(buffer, 4);
-		//std::cout << buffer << '\n'; //"data"	
-
-		wavout.read(buffer, 4);
-		//std::cout << ascii_seq_to_int_le(buffer, 4) << '\n'; //data size	
-		data_size = ascii_seq_to_int_le(buffer, 4);
-	}
-	else {
-		throw; //кидать ошибку об не открытии файла
-	}
-}
-*/
-
 void wavheader::genarate_header(std::ofstream& f) {
-	writer writer;
+	WavWriter writer;
 
 	f.write("RIFF", 4);
 	writer.write_num_b(0, f, 4); //File size - 8
