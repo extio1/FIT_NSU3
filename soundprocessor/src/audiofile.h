8b3db188687728fb3cb80e2af156321ac3d27769
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:50:14 +0700

    Update audiofile.h

--- soundprocessor/src/audiofile.h
+++ soundprocessor/src/audiofile.h
@@ -5,17 +5,21 @@
 #include <memory>
 #include <iostream>
 
+class WavWriter;
+class sample;
+
 class wavheader{
 public:
 
 	wavheader();
-	void read_header(std::ifstream&);
-	void write_header(std::ofstream&);
+	void read_header(std::ifstream&, const char* = 0);
 	void genarate_header(std::ofstream&);
 	unsigned int get_sample_rate() const;
 	~wavheader();
 
 private:
+
+	std::string filename;
 
 	unsigned int RIFF_chunk_size; //размер секции RIFF, размер файла минус 8 байт.
 
@@ -27,76 +31,85 @@ private:
 	unsigned short block_align; //Количество байт на одну выборку.
 	unsigned short sign_bits_per_sample; //Величина указывает количество бит, формирующих каждую выборку сигнала. 
 									     //Обычно эта величина 8, 16, 24 или 32.
-
-	std::string extra_info_path; //путь до файла с доп информацией (секция LIST)
 };
+
 
 class wavdata {
 public:
 
 	wavdata();
-	//wavdata(std::fstream&); 
-
-	void find_begin(std::ifstream&); //должен кидать о неправильном хедере (не нашел сегмент данных)
-	void read(std::vector<int>&, std::ifstream&);
-	void write(std::vector<int>&, std::ofstream&);
-	void set_begin(unsigned int);
-	bool endof();
-
+	void find_begin(std::ifstream&, const char*);
+	void read(std::vector<unsigned int>&, std::ifstream&);
+	void write(std::vector<unsigned int>&, std::ofstream&);
+	void skip(int);
 	~wavdata();
-	void test();
 
 private:
 	
-	bool eof;
+	std::unique_ptr<WavWriter> writer;
+
 	unsigned int cur_pos;
 	unsigned int data_size;
-	unsigned int data_pos_begin; //сдвиг до начала секции данных (тут щас возможно надо будет сдвинуться на 1 символ вперед)
-	const int size_data_chunk; //размер читаемых данных за 1 шаг
+	unsigned int data_pos_begin;
+
+	const int size_data_chunk;
+	const int bytes_per_sample;
 
 };
+
 
 class audiofile {
 public:
 
 	audiofile();
-	//audiofile(std::fstream);
-
 	void read_header(std::string&);
 	void create_audiofile(const char*);
-	bool eof();
-
 	~audiofile();
+	
+protected:
 
 	std::unique_ptr<wavheader> wheader;
-
-protected:
 	std::unique_ptr<wavdata> wdata;
+
 };
 
 
 class inaudiofile : public audiofile {
 public:
+
 	inaudiofile();
 	inaudiofile(const char*);
 	inaudiofile(const std::string&);
 
-	inaudiofile& operator>>(std::vector<int>&); //читает wav файл (data) и отдает вектор по 512 unsigned int
+	unsigned int get_sample_rate() const;
+	bool eof();
+	void skip(int);
+	inaudiofile& operator>>(std::vector<unsigned int>&); //читает wav файл (data) и отдает вектор по 512 unsigned int
+	inaudiofile& operator>>(std::vector<sample>&);
 
 	~inaudiofile();
+
 private:
+
 	std::ifstream audiostr;
+
 };
+
 
 class outaudiofile : public audiofile {
 public:
+
 	outaudiofile();
 	outaudiofile(const std::string&);
 	outaudiofile(const char*);
 
-	outaudiofile& operator<<(std::vector<int>&);//пишет в wav файл (data) по 512 unsigned int
+	outaudiofile& operator<<(std::vector<unsigned int>&); //пишет в wav файл (data) 
+	outaudiofile& operator<<(std::vector<sample>&);
 
 	~outaudiofile();
+
 private:
+
 	std::ofstream audiostr;
+
 };
