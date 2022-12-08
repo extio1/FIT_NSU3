648167270fcc608de09caaf1a88a28d025d7ae98
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-08 21:55:44 +0700

    Update sample.cpp

--- soundprocessor/src/sample.cpp
+++ soundprocessor/src/sample.cpp
@@ -1,14 +1,34 @@
 #include "sample.h"
+#include <climits>
 
 sample::sample(): data(0) {}
-sample::sample(int d){
-	if (d > 32760) {
-		d -= 32760;
-		d = -d;
+sample::sample(unsigned int d){
+	if (d > SHRT_MAX) {
+		data = d - USHRT_MAX;
 	}
-	data = d;
+	else {
+		data = d;
+	}
+	
 }
 sample::sample(const sample& other): data(other.data) {}
+
+void sample::normalize() {
+	if (data < SHRT_MIN) {
+		data = SHRT_MIN;
+	}
+	else if (data > SHRT_MAX) {
+		data = SHRT_MAX;
+	}
+}
+void sample::normalize(int& d) {
+	if (d < SHRT_MIN) {
+		d = SHRT_MIN;
+	}
+	else if (data > SHRT_MAX) {
+		d = SHRT_MAX;
+	}
+}
 
 sample& sample::operator=(const sample& other) {
 	if (this != &other) {
@@ -19,17 +39,34 @@ sample& sample::operator=(const sample& other) {
 
 sample& sample::operator+=(const sample& other) {
 	int sum = data + other.data;
-	if (sum < -32760) {
-		sum = -32760;
-	}
-	else if (sum > 32760) {
-		sum = 32760;
-	}
-
+	normalize(sum);
 	data = sum;
+
 	return *this;
 }
 
+sample& sample::operator/=(const sample& other) {
+	data /= other.data;
+	return *this;
+}
+
+sample& sample::operator*=(int p) {
+	data *= p;
+	normalize();
+	return *this;
+}
+
+sample& sample::operator*=(float p) {
+	data *= p;
+	normalize();
+	return *this;
+}
+
+sample operator/(const sample& op1, const sample& op2) {
+	sample temp = op1;
+	temp /= op2;
+	return temp;
+}
 
 sample operator+(const sample& op1, const sample& op2) {
 	sample temp = op1;
@@ -37,12 +74,26 @@ sample operator+(const sample& op1, const sample& op2) {
 	return temp;
 }
 
-sample::operator int() const {
-	int sampleint = data;
-	if (sampleint < 0) {
-		sampleint = -data + 32760;
+sample operator*(const sample& op1, int op2) {
+	sample temp = op1;
+	temp *= op2;
+	return temp;
+}
+
+sample operator*(const sample& op1, float op2) {
+	sample temp = op1;
+	temp *= op2;
+	return temp;
+}
+
+sample::operator unsigned int() const {
+	if (data < 0) {
+		return data + USHRT_MAX;
 	}
-	return sampleint;
+	else {
+		return data;
+	}
+	
 }
 
 sample::~sample() {}
