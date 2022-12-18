cb5674c7a45250b9fc81c4c2cd875d49a4e5b044
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-18 13:56:19 +0700

    Update mute.cpp

--- soundprocessor/src/mute.cpp
+++ soundprocessor/src/mute.cpp
@@ -11,30 +11,32 @@ void mute::launch(const command_info& com) {
 	inaudiofile inf(com.input[0]);
 	outaudiofile outf(com.output);
 
-	unsigned int time_sec = 0;
-	int sample_rate = inf.get_sample_rate();
-	int tick_counter = 0;
-
+	unsigned int sample_rate = inf.get_sample_rate();
+	start_mute = com.param[0];
+	end_mute = com.param[1];
 	std::vector<unsigned int> temp;
+
 	while (!inf.eof()) {
 		inf >> temp;
-
-		for (unsigned int& i : temp) {
-			if (tick_counter >= sample_rate) {
-				time_sec += 1;
-				tick_counter = 0;
-			}
-			if (time_sec >= com.param[0] && time_sec < com.param[1])
-				i = 0;
-			tick_counter++;
-		}
-
+		convert(temp, sample_rate);
 		outf << temp;
+	}
+}
+
+void mute::convert(std::vector<unsigned int>& temp, unsigned int sample_rate) {
+	for (unsigned int& i : temp) {
+		if (tick_counter >= sample_rate) {
+			time_sec += 1;
+			tick_counter = 0;
+		}
+		if (time_sec >= start_mute && time_sec < end_mute)
+			i = 0;
+		tick_counter++;
 	}
 }
 
 void mute::who_am_i() {
 	std::cout << "=====================================================\n";
-	std::cout << "$ mute <second_begin> <second_end> \nMute .wav file from <second_begin> to <second_end> second. \n";
-	std::cout << "=====================================================\n\n";
+	std::cout << "$ mute <second_begin> <second_end>\nMute .wav file from <second_begin> to <second_end> second.\n";
+	std::cout << "=====================================================\n";
 }
