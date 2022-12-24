6ecc87c7efe9a4f57d0d881f2f1865dcb12fb119
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-24 20:25:09 +0700

    Update csvparser.h

--- csvparser/csvparser.h
+++ csvparser/csvparser.h
@@ -4,25 +4,26 @@
 #include <vector>
 #include <fstream>
 #include <iterator>
+#include <memory>
 #include "expts.h"
+#include "tupleio.h"
+
 
 template <typename... TupleTypes>
 class CsvParser {
 private:
-	char cell_delim;
-	char line_delim;
-	char screen_sym;
+	Format format;
 	std::size_t offset;
 	std::ifstream& input_stream;
 	std::tuple<TupleTypes...>* line_tuple;
-	std::string line_str;
 	
 	class CsvIterator : public std::input_iterator_tag {
 	public:
-		CsvIterator(const bool s, CsvParser& c) : state(s), container(c) {}
+		CsvIterator(const bool s, CsvParser& c) : state(s), container(c) { }
 		CsvIterator(const bool s, std::size_t offset, CsvParser& c) : state(s), container(c) {
+			std::string line_str;
 			for (int i = 0; i < offset; i++) {
-				getline(container.input_stream, container.line_str);
+				getline(container.input_stream, line_str);
 				if (container.input_stream.eof()) {
 					throw offset_error();
 				}
@@ -38,7 +39,10 @@ private:
 		}
 
 		CsvIterator& operator++() {
-			state = container.read_next_line();
+			container.read_next_line();
+			if (container.input_stream.eof()) {
+				state = 0;
+			}
 			return *this;
 		}
 
@@ -66,34 +70,35 @@ private:
 		~CsvIterator() {}
 	private:
 		bool state; // state = 1 : there's something to read. 
-				    // state = 0 : the end of file has been reached
+					// state = 0 : the end of file has been reached
 		CsvParser& container;
 	};
-	
+
 	bool read_next_line() {
-		std::getline(input_stream, line_str);
+		auto new_tuple_ptr = read_tuple(input_stream, line_tuple, format);
 		if (input_stream.eof())
 			return 0;
-		std::tuple<TupleTypes...>* new_line_tuple = new std::tuple<TupleTypes...>;
-		*new_line_tuple = { 2, 2 };
-		line_tuple = new_line_tuple;
+
+		line_tuple = new_tuple_ptr;
+
 		return 1;
-	}/*
-	void parse_line() {
-		line_tuple<int, int> = { 1, 2 };
-	}*/
-	
+	}
+
 	friend CsvIterator;
 public:
-	
+
 	explicit CsvParser(std::ifstream& f, std::size_t o = 0, char cd = ',', char ld = '\n', char ss = '"') :
-		input_stream(f), offset(o), line_tuple(nullptr), cell_delim(cd), line_delim(ld), screen_sym(ss) {}
-	CsvIterator begin() { 
-		CsvIterator it = CsvIterator(true, offset, *this);
-		read_next_line(); 
-		return it; 
+		input_stream(f), offset(o), format(cd, ld, ss) {
+		line_tuple = new std::tuple<TupleTypes...>;
 	}
+
+	CsvIterator begin() {
+		CsvIterator it = CsvIterator(true, offset, *this);
+		read_next_line();
+		return it;
+	}
+
 	CsvIterator end() { return CsvIterator(false, *this); }
+	~CsvParser(){ delete line_tuple; }
+
 };
-
-
