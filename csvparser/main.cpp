c261aa1b731ade9692cca6da65b457ebe5f12246
extio1 <99738815+extio1@users.noreply.github.com>
2022-12-25 15:01:22 +0700

    Update main.cpp

--- csvparser/main.cpp
+++ csvparser/main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <fstream>
 #include <tuple>
 #include <string>
+#include <sstream>
 #include "tupleio.h"
+#include "csvparser.h"
 
 int main()
 {
-    std::tuple<std::string, int, double, char, float> test = { "HAHAHA,_The_JOKER's_TRAP", 11, -1.222, 'H', 2.87};
-    std::cout << test;
+    //std::tuple<std::string, int, double, char, float> test = { "HAHAHA,_The_JOKER's_TRAP", 11, -1.222, 'H', 2.87};
+    //std::cout << test << '\n';
+    
+    std::ifstream file("test.csv");
+    CsvParser<int, std::string, char, float> parser(file, 0 /*skip first lines count*/);
+    /*
+    int a;
+    std::cin >> a;
+    std::cout << '\n' << a;
+
+   */
+    parser.configure(';');
+    for (std::tuple<int, std::string, char, float> rs : parser) {
+        std::cout << rs << '\n';
+    }
+
+    /*std::tuple<int, int>* tu = new std::tuple<int, int>;
+    std::tuple<int, int> pu = { 99, 11 };
+    int* a = &std::get<0>(pu);
+    *a = 1000;
+    std::cout << pu;*/
+    /*
+    int a = 105;
+    void* ptr = &a;
+    int b = 12;
+    int&& b1 = 13;
+    int* bp = &b;
+    ptr = &b1;
+    std::cout << a;
+    */
+
 }
