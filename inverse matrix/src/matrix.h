405a96d6d56fee76c35b32c9bb3ab2eeed3ac0e1
Timofey Shalnev <99738815+extio1@users.noreply.github.com>
2023-03-19 11:04:19 +0700

    Update matrix.h

--- inverse matrix/src/matrix.h
+++ inverse matrix/src/matrix.h
@@ -1,13 +1,16 @@
 #pragma once
+#include <iostream>
+
 class Matrix {
 public:
 	Matrix();
-	Matrix(int size);
+	Matrix(const size_t size);
+	Matrix(const size_t size, const char* path);
 	Matrix(const Matrix& other);
 	Matrix(Matrix&& other) noexcept;
 
+	void read_from_file(const char* path);
 	void entry_arr();
-	void print_matrix() const;
 	int get_size() const;
 
 	Matrix& operator=(const Matrix& other);
@@ -26,8 +29,11 @@ public:
 
 	~Matrix();
 private:
-	int n; //размерность квадратной матрицы
+	size_t n; //размерность квадратной матрицы
 	float* mat; //сама матрица записанна в одномерный массив строка за строкой
+
+	void read_by_stream(std::istream& stream);
 };
 
-Matrix inverse_matrix(const Matrix&, const int);
+Matrix inverse_matrix(const Matrix&, const size_t);
+std::ostream& operator<<(std::ostream &os, const Matrix& matrix);
